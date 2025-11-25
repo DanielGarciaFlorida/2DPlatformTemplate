cf0a75353aaaa3dc450fb3dd598ca3d55eeb2457
jjorgeProjects <jjorge@florida-uni.es>
2025-11-25 07:24:02 +0100

    Jump interaction with New Input System

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
+//Import namespace to use callback
+using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
+    public float jumpForce = 10.0f;
+    Rigidbody2D rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    /*
+    //Should be public
+    public void Jump()
+    {
+        //This method is called 3 times:
+        // - Starter: Starting Key down
+        // - Performed: Performed Key down 
+        // - Canceled: When key is released
+        Debug.Log("Jumping");
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+    */
+    public void Jump(InputAction.CallbackContext callbackContext)
+    {
+        //This method is called 3 times:
+        // - Starter: Starting Key down
+        // - Performed: Performed Key down 
+        // - Canceled: When key is released
+        if (callbackContext.performed) { 
+            Debug.Log("Jumping");
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
